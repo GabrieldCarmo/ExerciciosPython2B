edb6f10f4e7001c2521bd04d02c7cea850250cfa
Gabriel do Carmo <gabrieldcarmo1@gmail.com>
2026-05-08 10:39:56 -0300

    7 - Finalizando o exercicio 23

--- ex23.py
+++ ex23.py
@@ -2,18 +2,24 @@ listaAluno = []
 qntdAprovada = 0
 qntdReprovada = 0
 qntdRecuperacao = 0
+
+print("=-=-" * 15)
+print("NSA 2.0.0")
+print("=-=-" * 15)
+
 def calcularNota(indice):
     global qntdAprovada, qntdReprovada, qntdRecuperacao
     nota = listaAluno[indice]["notaAluno"]
     if nota>= 7:
         qntdAprovada = qntdAprovada + 1
-        listaAluno[indice]["statusAluno"] = "Aprovado"
+        listaAluno[indicae]["statusAluno"] = "Aprovado"
     elif nota >= 5:
         qntdRecuperacao = qntdRecuperacao+ 1
         listaAluno[indice]["statusAluno"] = "Recuperação"
     else:
         qntdReprovada = qntdReprovada + 1
         listaAluno[indice]["statusAluno"] = "Reprovado"
+        
 def calcularMedia():
     i = 0
     totalNotas = 0
@@ -21,41 +27,21 @@ def calcularMedia():
         totalNotas = totalNotas + listaAluno[i]["notaAluno"]
         i = i + 1
     media = totalNotas/len(listaAluno)
-    return (media)
-
-def calcularClassificacao():
-    global maiorNota, menorNota, melhoresAlunos, pioresAlunos
-    i = 1
-    maiorNota = listaAluno[0]["notaAluno"]
-    menorNota = listaAluno[0]["notaAluno"]
-    melhoresAlunos = [listaAluno[0]["nomeAluno"]]
-    pioresAlunos = [listaAluno[0]["nomeAluno"]]
-    while i<len(listaAluno):
-        nota = listaAluno[i]["notaAluno"]
-        if nota>maiorNota:
-            maiorNota = nota
-            melhoresAlunos.clear()
-            melhoresAlunos.append(listaAluno[i]["nomeAluno"])
-        elif nota==maiorNota:
-            melhoresAlunos.append(listaAluno[i]["nomeAluno"])
-        if nota<menorNota:
-            menorNota = nota
-            pioresAlunos.clear()
-            pioresAlunos.append(listaAluno[i]["nomeAluno"])
-        elif nota == menorNota:
-            pioresAlunos.append(listaAluno[i]["nomeAluno"])
+    return round(media)
+     
 while True:
     while True:
+        c = 0
         try:
             nome = input("Digite o nome do aluno: ")
-            idade = int(input("Digite a idade do aluno: "))
-            nota = float(input("Digite a nota do aluno: "))
+            idade = int(input("Digite a idade de " + nome + ": "))
+            nota = float(input("Digite a nota de " + nome + ": "))
             if idade>0 and nota>=0 and nota<=10:
                 break
             else:
-                print("Idade ou nota invalida, tente novamente")
+                print("\nIdade ou nota invalida, tente novamente\n")
         except ValueError:
-            print("Valor invalido, insira novamente")
+            print("\nInsira a idade em números. Tente novamente.\n")
     Aluno = {
         "nomeAluno": nome,
         "idadeAluno": idade,
@@ -64,38 +50,62 @@ while True:
     }
     listaAluno.append(Aluno)
 
-    resposta = input("Digite 'N' se você deseja para de cadastrar")
+    resposta = input("Digite 'N' para parar de cadastrar. Caso contrário aperte enter.")
     if resposta.lower() == "n":
         break
     else:
-        print("Cadastrando novo aluno:")
+        print("\nCadastrando novo aluno:")
 j = 0
 while j<len(listaAluno):
     calcularNota(j)
     j += 1
 
+maiorNota = 0
+menorNota = 0
+melhoresAlunos = []
+pioresAlunos = []
+
+i = 1
+maiorNota = listaAluno[0]["notaAluno"]
+menorNota = listaAluno[0]["notaAluno"]
+melhoresAlunos = [listaAluno[0]["nomeAluno"]]
+pioresAlunos = [listaAluno[0]["nomeAluno"]]
+
+while i<len(listaAluno):
+    nota = listaAluno[i]["notaAluno"]
+    if nota>maiorNota:
+        maiorNota = nota
+        melhoresAlunos=[listaAluno[i]["nomeAluno"]]
+    elif nota==maiorNota:
+        melhoresAlunos.append(listaAluno[i]["nomeAluno"])
+    if nota<menorNota:
+        menorNota = nota
+        pioresAlunos=[listaAluno[i]["nomeAluno"]]
+    elif nota == menorNota:
+        pioresAlunos.append(listaAluno[i]["nomeAluno"])
+    i = i + 1
+
 #Mostrando o resultado
 i = 0
 while i<len(listaAluno):
-    print("Nome: " + listaAluno[i]["nomeAluno"])
+    print("\nNome: " + listaAluno[i]["nomeAluno"])
     print("Idade: ", listaAluno[i]["idadeAluno"])
     print("Nota: ", listaAluno[i]["notaAluno"])
-    print("Este aluno está atualmente " + listaAluno[i]["statusAluno"])
+    print("Situação:  " + listaAluno[i]["statusAluno"])
     i += 1
-print("A media da turma foi: ", calcularMedia())
-print("A quantidade de aprovados foi: ", qntdAprovada)
-print("A quantidade de reprovados foi: ", qntdReprovada)
-print("A quantidade de pessoas em recuperação foi: ", qntdRecuperacao)
 
-calcularClassificacao()
-print("A menor nota da turma foi: ", menorNota, " com esse(s) aluno(s) que tem essa nota: ")
+print("\nmedia da turma foi: ", calcularMedia())
+print("Aprovados: ", qntdAprovada)
+print("Reprovados: ", qntdReprovada)
+print("Recuperação: ", qntdRecuperacao)
+
+print("\nA menor nota da turma: ", menorNota, "do aluno: ")
 i = 0
 while i<len(pioresAlunos):
     print(pioresAlunos[i])
     i = i + 1
-print("E a maior nota foi: ", maiorNota, "com esse(s) aluno(s) que tem essa nota: ")
+print("\nE a maior nota foi: ", maiorNota, "do aluno: ")
 i = 0
 while i<len(melhoresAlunos):
     print(melhoresAlunos[i])
     i = i + 1
-
